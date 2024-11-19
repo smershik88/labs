a6282646a12c3c661dd79cc8c719581b4317ed46
smershik88 <144458201+smershik88@users.noreply.github.com>
2024-11-19 10:55:34 +0300

    Add files via upload

---
+++ lab_3_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string mystr;
+    std::cout << " What ’s your name ? ";
+    std::getline(std::cin, mystr);
+    std::cout << " Hello " << mystr << " .\n ";
+    std::cout << " What is your favorite team ? ";
+    std::getline(std::cin, mystr);
+    std::cout << " I like " << mystr << " too !\n ";
+    return 0;
+}
